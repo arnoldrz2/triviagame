a0b1e0ad19152a19cf035522fff061b0f33c5ddf
arnold <arnoldrz2@gmail.com>
2017-11-30 01:00:47 -0600

    added start button function and countdown function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,21 @@
+
+
 // Trival countdown timer start number
-var number = 13;
+var number = 31;
 
 // Variable that will hold the interval ID
 // when "run" function is executed.
-var = intervalId;
+var intervalId;
 
-// Set decrenment function to run on window load
-window.onload = function() {
+//When Start button is clicked, this function will execute the run function.
+$('#start').on("click", run);
+
+
+// Set decrenment function to run on start button click
+function run() {
   intervalId = setInterval(decrement, 1000);
 }
+
 
 // When window loads, this function will be executed
 function decrement() {
@@ -16,5 +23,19 @@ function decrement() {
   //Decrease number by one.
   number--;
 
-  
+  // Display number on page
+  $('#timer').html("<h2>" + number + "</h2>");
+
+  //Once the timer hits zero, the function will stop.
+  if (number === 0) {
+
+    stop();
+    alert("Time's up, homie.");
+  }
+}
+
+//Stop function
+function stop() {
+
+  clearInterval(intervalId);
 }
