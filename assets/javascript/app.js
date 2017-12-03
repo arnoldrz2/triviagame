77177f2eb60d1f5d8731c2a8b88d9746108d6200
arnold <arnoldrz2@gmail.com>
2017-12-03 00:49:08 -0600

    Updated javascript scoring system and added scoring evaluation code.  Updated html/css style a bit as well

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,7 @@
+//LIT TRIVIA GAME: Lit = true and Trippin = false. Max clout (score) level is 6,
+// which means the user picked the correct option for every statement.
+// I hope you don't mind the silly theme. :)
+
 // Trivia game countdown timer start number
 var number = 31;
 
@@ -5,20 +9,29 @@ var number = 31;
 // when "run" function is executed.
 var intervalId;
 
-//Points Counter will tally how many answers are correct
-//It'll start of with 0
-var points = 0;
+//As the user makes choices (true="lit and false="trippin") which
+// will increase the counters on correctAnswers and incorrectAnswers.
+//They'll each start with 0 points.
+var correctAnswers = 0;
+var incorrectAnswers = 0;
+
+// Initial "checked" values
+var checkedOne = false;
+var checkedTwo = false;
+var checkedThree = false;
+var checkedFour = false;
+var checkedFive = false;
+var checkedSix = false;
 
 
 //When Start button is clicked, this function will execute the run function.
 $('#start').on("click", run);
-
-
   //Set decrenment function to run on start button click
   //Function will also reset countdown timer and hide start button
   function run() {
     number = 31;
-    points = 0;
+    correctAnswers = 0;
+    incorrectAnswers = 0;
     $('#start').hide();
     intervalId = setInterval(decrement, 1000);
   }
@@ -26,23 +39,18 @@ $('#start').on("click", run);
 
   // When window loads, this function will be executed
   function decrement() {
-
     //Decrease number by one.
     number--;
-
     // Display number on page
     $('#timer').html("<h2>" + number + "</h2>");
-
     //Once the timer hits zero, the function will stop.
     //Also, alert user that time is up, show start button again, and reset timer.
     if (number === 0) {
-
       stop();
       alert("On no! Time's up, homie. The haters have won. :(");
       location.reload();
     }
   }
-
   //Stop function
   function stop() {
     clearInterval(intervalId);
@@ -57,22 +65,22 @@ $('#start').on("click", run);
   $(".choice-1").on("click", function firstAnswer() {
       var radioOne = document.getElementsByName("choice1");
       var i = 0, lenOne = radioOne.length;
-      var checkedOne = false;
       var userAnswerOne;
-
+      //This records user input's value
       for(; i < lenOne; i++){
         if(radioOne[i].checked) {
           checkedOne = true;
           userAnswerOne = radioOne[i].value;
         }
       }
-      //Correct answer will add points to the user's score.
-      if(userAnswerOne === "lit") {
-        points++;
+      //The User's answer will either add points to trueChoices or falseChoices
+      if(userAnswerOne === "Lit") {
+        correctAnswers++;
       }
-      else {
-        points--;
+      else if (userAnswerOne === "Trippin") {
+        incorrectAnswers++;
       }
+      $("form1").html(userAnswerOne);
       endGame();
     });
 
@@ -81,7 +89,6 @@ $('#start').on("click", run);
   $(".choice-2").on("click", function secondAnswer() {
       var radioTwo = document.getElementsByName("choice2");
       var i = 0, lenTwo = radioTwo.length;
-      var checkedTwo = false;
       var userAnswerTwo;
 
       for(; i < lenTwo; i++){
@@ -90,13 +97,14 @@ $('#start').on("click", run);
           userAnswerTwo = radioTwo[i].value;
         }
       }
-      //Correct answer will add points to the user's score.
-      if(userAnswerTwo === "trippin") {
-        points++;
+      //The User's answer will either add points to trueChoices or falseChoices
+      if(userAnswerTwo === "Trippin") {
+        correctAnswers++;
       }
-      else {
-        points--;
+      else if (userAnswerTwo = "Lit") {
+        incorrectAnswers++;
       }
+      $("form2").html(userAnswerTwo);
       endGame();
     });
 
@@ -105,7 +113,6 @@ $('#start').on("click", run);
   $(".choice-3").on("click", function thirdAnswer() {
       var radioThree = document.getElementsByName("choice3");
       var i = 0, lenThree = radioThree.length;
-      var checkedThree = false;
       var userAnswerThree;
 
       for(; i < lenThree; i++){
@@ -114,13 +121,14 @@ $('#start').on("click", run);
           userAnswerThree = radioThree[i].value;
         }
       }
-      //Correct answer will add points to the user's score.
-      if(userAnswerThree === "lit") {
-        points++;
+      //The User's answer will either add points to trueChoices or falseChoices
+      if(userAnswerThree === "Lit") {
+        correctAnswers++;
       }
-      else {
-        points--;
+      else if (userAnswerThree === "Trippin") {
+        incorrectAnswers++;
       }
+      $("form3").html(userAnswerThree);
       endGame();
     });
 
@@ -129,7 +137,6 @@ $('#start').on("click", run);
   $(".choice-4").on("click", function forthdAnswer() {
       var radioFour = document.getElementsByName("choice4");
       var i = 0, lenFour = radioFour.length;
-      var checkedFour = false;
       var userAnswerFour;
 
       for(; i < lenFour; i++){
@@ -138,13 +145,14 @@ $('#start').on("click", run);
           userAnswerFour = radioFour[i].value;
         }
       }
-      //Correct answer will add points to the user's score.
-      if(userAnswerFour === "lit") {
-        points++;
+      //The User's answer will either add points to trueChoices or falseChoices
+      if(userAnswerFour === "Lit") {
+        correctAnswers++;
       }
-      else {
-        points--;
+      else if (userAnswerFour === "Trippin") {
+        incorrectAnswers++;
       }
+      $("form4").html(userAnswerFour);
       endGame();
     });
 
@@ -153,7 +161,6 @@ $('#start').on("click", run);
     $(".choice-5").on("click", function fifthAnswer() {
         var radioFive = document.getElementsByName("choice5");
         var i = 0, lenFive = radioFive.length;
-        var checkedFive = false;
         var userAnswerFive;
 
         for(; i < lenFive; i++){
@@ -162,13 +169,14 @@ $('#start').on("click", run);
             userAnswerFive = radioFive[i].value;
           }
         }
-        //Correct answer will add points to the user's score.
-        if(userAnswerFive === "trippin") {
-          points++;
+        //The User's answer will either add points to trueChoices or falseChoices
+        if(userAnswerFive === "Trippin") {
+          correctAnswers++;
         }
-        else {
-          points--;
+        else if (userAnswerFive === "Lit") {
+          incorrectAnswers++;
         }
+        $("form5").html(userAnswerFive);
         endGame();
       });
 
@@ -177,7 +185,6 @@ $('#start').on("click", run);
       $(".choice-6").on("click", function sixthAnswer() {
           var radioSix = document.getElementsByName("choice6");
           var i = 0, lenSix = radioSix.length;
-          var checkedSix = false;
           var userAnswerSix;
 
           for(; i < lenSix; i++){
@@ -186,21 +193,49 @@ $('#start').on("click", run);
               userAnswerSix = radioSix[i].value;
             }
           }
-          //Correct answer will add 1 point to the user's score.
-          if(userAnswerSix === "lit") {
-            points++;
+          //The User's answer will either add points to trueChoices or falseChoices
+          if(userAnswerSix === "Lit") {
+            correctAnswers++;
           }
-          //Incorrect answer will subtract -1 point from user's score.
-          else {
-            points--;
+          else if (userAnswerSix === "Trippin") {
+            incorrectAnswers++;
           }
+          $("form6").html(userAnswerSix);
           endGame();
         });
 
-//Evaluate score
+//Score evaluations and score alerts.
 function endGame() {
-  if(points === 6 && number > 0) {
-    alert("Fuego alert! Your lit level is turnt up to the max. Great job, fam!")
+  if(correctAnswers == 6 && incorrectAnswers == 0 && number > 0) {
+    alert("You scored a clout level of 6 out 6.")
+    alert("Fuego alert! Your clout level is turnt up to the max. Great job, fam!")
+    location.reload();
+  }
+  else if(correctAnswers == 5 && incorrectAnswers == 1 && number > 0) {
+    alert("You scored a clout level of 5 out of 6.")
+    alert("Nice clout game, homie.")
+    location.reload();
+  }
+  else if(correctAnswers == 4 && incorrectAnswers == 2 && number > 0) {
+    alert("You scored a clout level of 4 out of 6.")
+    alert("Nice clout game, homie.")
+    location.reload();
+  }
+  else if(correctAnswers == 3 && incorrectAnswers == 3 && number > 0) {
+    alert("You scored a clout level of 3 out of 6.")
+    location.reload();
+  }
+  else if(correctAnswers == 2 && incorrectAnswers == 4 && number > 0) {
+    alert("You scored a clout level of 2 out of 6.")
+    location.reload();
+  }
+  else if(correctAnswers == 1 && incorrectAnswers == 5 && number > 0) {
+    alert("You scored a clout level of 1 out of 6.")
+    location.reload();
+  }
+  else if(correctAnswers == 0 && incorrectAnswers == 6 && number > 0) {
+    alert("You scored a clout level of 0 out of 6.")
+    alert("Woah...")
     location.reload();
   }
 }
